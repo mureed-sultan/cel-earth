3512ee54141f5fa50ad58d9c3515161ea8802ce2
Mureed Sultan <91979102+Mureed-Sultan-Geni@users.noreply.github.com>
2022-08-03 13:54:54 +0500

    new update

---
+++ src/scripts/app/site/pages/data.js
@@ -0,0 +1,39 @@
+export const rumiBooks = {
+    book1: {
+        image: '/images/cal-earth/book(1).png',
+        title: 'The Spritual poems of Rumi',
+        price: '16.99'
+    },
+    book2: {
+        image: '/images/cal-earth/book(2).png',
+        title: 'The friendship poems of rumi',
+        price: '16.99'
+    },
+    book3: {
+        image: '/images/cal-earth/book(3).png',
+        title: 'Love poems of Rumi',
+        price: '16.99'
+    },
+};
+export const recentNews = {
+    news1: {
+        image: '/images/cal-earth/news-1.jpg',
+        title: 'Super Adobe ICC approval complete',
+        publish: 'June.20,2021'
+    },
+    news2: {
+        image: '/images/cal-earth/news-2.jpg',
+        title: `super adobe spotligh: tommy's tea dome`,
+        publish: 'March.24,2021'
+    },
+    news3: {
+        image: '/images/cal-earth/news-3.jpg',
+        title: 'Luxuarious super adobe through the lens of hesse benson',
+        publish: 'Febuary.24,2021'
+    },
+    news4: {
+        image: '/images/cal-earth/news-4.jpg',
+        title: 'Down to earth with calearth by goodee',
+        publish: 'Febuary.24,2021'
+    },
+};
--- src/scripts/app/site/pages/home.js
+++ src/scripts/app/site/pages/home.js
@@ -1,25 +1,9 @@
 import getComp from 'setjs/template/component.js';
-let rumiBooks={
-  book1:{
-    image:'/images/cal-earth/book(1).png',
-    title:'The Spritual poems of Rumi',
-    price:'16.99'
-  },   
-  book2:{
-    image:'/images/cal-earth/book(2).png',
-    title:'The friendship poems of rumi',
-    price:'16.99'
-  },    
-  book3:{
-    image:'/images/cal-earth/book(3).png',
-    title:'Love poems of Rumi',
-    price:'16.99'
-  },  
-};
+import {rumiBooks, recentNews} from './data.js';
 export default {
   templates:['site/home'],
   comp:()=>{
-    let page = getComp('site/home',{rumiBooks});
+    let page = getComp('site/home',{rumiBooks,recentNews});
     return page;
   }
 };
